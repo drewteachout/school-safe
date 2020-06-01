b80a13b7643f95cc7d2a626c08edc6fba0544839
drewteachout <drewteachout@gmail.com>
2020-06-01 19:02:00 -0400

    Added symptom checklist and a state for data

--- components/SurveyScreen.js
+++ components/SurveyScreen.js
@@ -1,31 +1,33 @@
 import React, {useState} from 'react';
 import { View } from 'react-native';
 import { Button, CheckBox, Divider, Input, Text, ListItem } from 'react-native-elements';
+
 import { styles } from './config';
-
-
 
 export function SurveyScreen({ navigation }) {
 	const [state, setState] = useState([
-		{ key: 0, text: 'Symptom A', checked: false },
-		{ key: 1, text: 'Symptom B', checked: false },
-		{ key: 2, text: 'Symptom C', checked: false },
-		{ key: 3, text: 'Symptom D', checked: false },
-		{ key: 4, text: 'Symptom E', checked: false }
+		{ id: 0, text: 'Symptom A', checked: false },
+		{ id: 1, text: 'Symptom B', checked: false },
+		{ id: 2, text: 'Symptom C', checked: false },
+		{ id: 3, text: 'Symptom D', checked: false },
+		{ id: 4, text: 'Symptom E', checked: false }
 	]);
-	// this.state = [
-	// 	{ text: 'Symptom A', checked: false },
-	// 	{ text: 'Symptom B', checked: false },
-	// 	{ text: 'Symptom C', checked: false },
-	// 	{ text: 'Symptom D', checked: false },
-	// 	{ text: 'Symptom E', checked: false }
-	// ]
 
-	const changeState = (id) => {
-		console.log('change state', id)
-			// setState(prevState => {
-			// 	console.log(prevState)
-			// })
+	// TODO: Implement state for name on form
+	// const [name, setName] = useState([])
+
+	const changeState = i => {
+		setState(prevState => {
+			var newState =[];
+			prevState.map(item => {
+				if (item.id == i) {
+					newState.push({ id: item.id, text: item.text, checked: !item.checked});
+				} else {
+					newState.push({ id: item.id, text: item.text, checked: item.checked});
+				}
+			})
+			return newState;
+		});
 	}
 
 	return (
@@ -34,11 +36,12 @@ export function SurveyScreen({ navigation }) {
 			<Text style={styles.text}>Has your child experienced any of theses symptoms in the last 14 days? Please check all that apply.</Text>
 			<Divider style={{ backgroundColor: '#005B82'}} />
 			{
-				state.map((item) => (
+				state.map((item, i) => (
 					<CheckBox
+						key={i}
 						title={item.text}
 						checked={item.checked}
-						onPress={changeState(item.key)}
+						onPress={() => changeState(i)}
 					/>
 				))
 			}
