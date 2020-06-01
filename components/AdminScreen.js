33cf32ca41788d170d464c16a832938ae281a568
drewteachout <drewteachout@gmail.com>
2020-06-01 15:54:06 -0400

    Fixed icon display

--- components/AdminScreen.js
+++ components/AdminScreen.js
@@ -1,11 +1,30 @@
 import React from 'react';
 import { View, Text, Button } from 'react-native';
+import { ListItem } from 'react-native-elements';
+import Icon from 'react-native-vector-icons/FontAwesome';
 import { styles } from './config';
 
 export function AdminScreen() {
 	return (
-		<View style={styles.container}>
-			<Text>Admin Page Currently Not Available</Text>
+		<View>
+			<ListItem 
+				title="Permitted"
+				bottomDivider
+				chevron
+				badge={{ status: 'success', value: 75 }}
+			/>
+			<ListItem
+				title="Banned"
+				bottomDivider
+				chevron
+				badge={{ status: 'error', value: 2 }}
+			/>
+			<ListItem
+				title="Incomplete"
+				bottomDivider
+				chevron
+				badge={{ status: 'warning', value: 23 }}
+			/>
 		</View>
 	);
 }
--- components/SurveyScreen.js
+++ components/SurveyScreen.js
@@ -1,11 +1,47 @@
-import React from 'react';
-import { View, Text, Button } from 'react-native';
+import React, {useState} from 'react';
+import { View } from 'react-native';
+import { Button, CheckBox, Divider, Input, Text, ListItem } from 'react-native-elements';
 import { styles } from './config';
 
+
+
 export function SurveyScreen({ navigation }) {
+	const [state, setState] = useState([
+		{ key: 0, text: 'Symptom A', checked: false },
+		{ key: 1, text: 'Symptom B', checked: false },
+		{ key: 2, text: 'Symptom C', checked: false },
+		{ key: 3, text: 'Symptom D', checked: false },
+		{ key: 4, text: 'Symptom E', checked: false }
+	]);
+	// this.state = [
+	// 	{ text: 'Symptom A', checked: false },
+	// 	{ text: 'Symptom B', checked: false },
+	// 	{ text: 'Symptom C', checked: false },
+	// 	{ text: 'Symptom D', checked: false },
+	// 	{ text: 'Symptom E', checked: false }
+	// ]
+
+	const changeState = (id) => {
+		console.log('change state', id)
+			// setState(prevState => {
+			// 	console.log(prevState)
+			// })
+	}
+
 	return (
-		<View style={styles.container}>
-			<Text>Survey Screen</Text>
+		<View>
+			<Input labelStyle={styles.input} containerStyle={styles.input} inputContainerStyle={styles.input} inputStyle={styles.input} label='Student Name' />
+			<Text style={styles.text}>Has your child experienced any of theses symptoms in the last 14 days? Please check all that apply.</Text>
+			<Divider style={{ backgroundColor: '#005B82'}} />
+			{
+				state.map((item) => (
+					<CheckBox
+						title={item.text}
+						checked={item.checked}
+						onPress={changeState(item.key)}
+					/>
+				))
+			}
 			<Button
 				title="Submit"
 				onPress={() => navigation.popToTop()}
